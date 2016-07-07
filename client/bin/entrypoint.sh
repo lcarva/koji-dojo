d4822438e22fd2d13d65bc2fa529e142c6cbe8ac
Luiz Carvalho <lucarval@redhat.com>
2016-07-07 16:38:03 -0400

    Remove sshd from koji-client container

--- client/bin/entrypoint.sh
+++ client/bin/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-set -xeuo pipefail
+set -euo pipefail
 
-# TODO: Verify this is ok when restarting container
 yum -y localinstall /opt/koji/noarch/koji-1*.rpm
 
+rm -rf /root/{.koji,bin}
 mkdir -p /root/{.koji,bin}
-echo "Generating user-specific koji client links and configs"
+
 for userdir in $(ls -d /opt/koji-clients/*); do
 	user=$(basename $userdir)
-	echo "Adding: ${user} (${userdir})"
 
-	cat <<EOF >> /root/.koji/config
+    # Create a koji profile for each user
+	cat << EOF >> /root/.koji/config
 [koji-${user}]
 server = https://koji-hub/kojihub
 authtype = ssl
@@ -20,7 +20,7 @@ serverca = ${userdir}/serverca.crt
 
 EOF
 
-	echo "Linking koji to: /root/bin/koji-${user}"
+    # Create symbolic link for each user
 	ln -s /usr/bin/koji /root/bin/koji-${user}
 done
 
@@ -30,8 +30,7 @@ cat /root/.koji/config
 echo "Available user-specific koji commands are:"
 ls -1 /root/bin/koji-*
 
-ssh-keygen -t rsa -N '' -f /etc/ssh/ssh_host_rsa_key
-ssh-keygen -t dsa -N '' -f /etc/ssh/ssh_host_dsa_key
-
-for ip in `hostname -I`; do echo 'SSH:' $ip':22'; done
-/usr/sbin/sshd -D
+# Keep container around so "docker exec" can be
+# run against it, but gracefully exit on SIGTERM
+trap 'exit 0' SIGTERM
+while true; do sleep 2; done
